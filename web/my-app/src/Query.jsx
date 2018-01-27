23291a7264b008cae3fc784c485744d38965c704
Peter Hahn Linux <peterkunho@gmail.com>
2018-01-27 15:07:22 -0500

    Updated Query Page

--- web/my-app/src/Query.jsx
+++ web/my-app/src/Query.jsx
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
 
 class Query extends Component {
+    constructor(props){
+        super(props)
+        this.state = {date: '', food: ''};
+        this.handleChange = this.handleChange.bind(this); 
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+
+
+    }
 
     handleSubmit(event){
-        alert("ALERT BITCH")
+        alert("You did this date! " + this.state.date + " with this food: " + this.state.food)
         event.preventDefault();
     }
     handleSubmitTwo(event){
-        alert("ALERT 2 BItCH")
+        alert("You want this food! " + this.state.food)
+
         event.preventDefault();
     }
+
+    handleChange(event){
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
 
     render(){
         return (
@@ -17,14 +38,14 @@ class Query extends Component {
          <h1>Query Page</h1>
             <p>We can have this to query a date, to get a complete listing of all the food</p>
             <form onSubmit={this.handleSubmit}>
-                Date: <input type="date" name="date"/>
+                Date: 
+                <input type="date" value={this.state.date} name="date" onChange={this.handleChange}/><br/>
+                Food: 
+                <input type="text" name="food" onChange={this.handleChange}/>
+
                 <input type="submit" value="Submit"/>
             </form>
-            <p>We can have this to query a food, to get a list of dates of foods</p>
-            <form onSubmit={this.handleSubmitTwo}>
-                Food: <input type="text" name="food"/>
-                <input type="submit" value="Submit" />
-            </form>
+
         </div>
         )
     }

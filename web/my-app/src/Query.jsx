6a8e418799bc152dc840be2ca230349893a5196c
Peter Hahn Linux <peterkunho@gmail.com>
2018-01-27 19:38:28 -0500

    push

--- web/my-app/src/Query.jsx
+++ web/my-app/src/Query.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Query extends Component {
     constructor(props){
         super(props)
-        this.state = {date: '', food: ''};
+        this.state = {when: '', food: ''};
         this.handleChange = this.handleChange.bind(this); 
         this.handleSubmit = this.handleSubmit.bind(this);
 
@@ -12,7 +12,7 @@ class Query extends Component {
     }
 
     handleSubmit(event){
-        alert("You did this date! " + this.state.date + " with this food: " + this.state.food)
+        alert("You did this date! " + this.state.when + " with this food: " + this.state.food)
         event.preventDefault();
     }
 
@@ -33,11 +33,16 @@ class Query extends Component {
          <h1>Query Page</h1>
             <p>We can have this to query a date, to get a complete listing of all the food</p>
             <form onSubmit={this.handleSubmit}>
-                Date: 
-                <input type="date" value={this.state.date} name="date" onChange={this.handleChange}/><br/>
+                Start Date: 
+                <select name="when" onChange={this.handleChange}>
+                    <option value="today">Today</option>
+                    <option value="tomorrow">Tomorrow</option>
+                    <option value="week">This Week</option>
+                    <option value="twoweeks">Next Two Weeks</option>
+                </select><br/>
+
                 Food: 
                 <input type="text" name="food" onChange={this.handleChange}/>
-
                 <input type="submit" value="Submit"/>
             </form>
 
